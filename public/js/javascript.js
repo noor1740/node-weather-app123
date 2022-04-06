4cdfa6b0bded14137afe8937a1c20953bd597da2
noor1740 <103094140+noor1740@users.noreply.github.com>
2022-04-06 10:50:49 +0500

    Removed unnecessary console.log line

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -11,7 +11,7 @@ console.log('javascript is loaded up successfully')
 
 fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/.json?access_token=pk.eyJ1IjoibmtuaWF6aSIsImEiOiJjbDFkZzVhNGowYm9iM2pueGdpaWhocGY3In0.U887fosBi7X0GVPujlTHsQ&limit=1").then((res,error)=>{
 
-console.log(error)
+//console.log(error)
 res.json().then((data)=>{
     console.log(data)
 })
